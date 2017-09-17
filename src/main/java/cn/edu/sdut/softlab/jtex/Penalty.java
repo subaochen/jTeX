a1d0aed3d1b31fb67067ecd11eeeceb0918066b6
Su Baochen <subaochen@126.com>
2017-09-17 16:34:22 +0800

    first commit

---
+++ src/main/java/cn/edu/sdut/softlab/jtex/Penalty.java
@@ -0,0 +1,25 @@
+package cn.edu.sdut.softlab.jtex;
+
+/**
+ * Created by subaochen on 17-9-17.
+ */
+public class Penalty {
+    private int linePenalty;
+    private int hyphenPenalty;
+
+    public int getLinePenalty() {
+        return linePenalty;
+    }
+
+    public void setLinePenalty(int linePenalty) {
+        this.linePenalty = linePenalty;
+    }
+
+    public int getHyphenPenalty() {
+        return hyphenPenalty;
+    }
+
+    public void setHyphenPenalty(int hyphenPenalty) {
+        this.hyphenPenalty = hyphenPenalty;
+    }
+}
